4df4b291be4ac7dfab509b45fffadda54daa2bb7
Sebastien Lubrano <sebastienlubrano38@gmail.com>
2021-08-03 14:50:04 -0700

    updated tuning for 2016-17 honda accord

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -233,10 +233,10 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 3536. * CV.LB_TO_KG + STD_CARGO_KG
       ret.wheelbase = 2.75
       ret.centerToFront = ret.wheelbase * 0.39
-      ret.steerRatio = 13.37 # as spec
+      ret.steerRatio = 13.6 # 13.37 as spec
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 238], [0, 238]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.24], [0.08]]      
-      ret.lateralTuning.pid.kf = 0.00002
+      ret.lateralTuning.pid.kf = 0.000025
       tire_stiffness_factor = 0.8467
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
       ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5] 
