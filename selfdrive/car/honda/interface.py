5248f55c42a92137c8486b680c77dc887fdde4b6
Sebastien Lubrano <sebastienlubrano38@gmail.com>
2021-08-20 18:59:25 -0700

    added personal honda accord nidec hybrid tuning

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -249,7 +249,7 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 3536. * CV.LB_TO_KG + STD_CARGO_KG
       ret.wheelbase = 2.75
       ret.centerToFront = ret.wheelbase * 0.39
-      ret.steerRatio = 13.6 # 13.37 is spec
+      ret.steerRatio = 14.33 # 13.37 is spec
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 238], [0, 238]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.27], [0.07]]      
       ret.lateralTuning.pid.kf = 0.000025
@@ -486,7 +486,7 @@ class CarInterface(CarInterfaceBase):
     ret.stoppingControl = True
     ret.startAccel = 0.5
 
-    ret.steerActuatorDelay = 0.1
+    ret.steerActuatorDelay = 0.05
     ret.steerRateCost = 0.5
     ret.steerLimitTimer = 0.8
 
