bcfc47400f35b184873460bd2772e20356324eeb
Sebastien Lubrano <sebastienlubrano38@gmail.com>
2021-08-26 15:09:42 -0700

    removed 'laneless hack', updated tuning for 2017 honda accord hybrid nidec

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -207,7 +207,7 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = ret.wheelbase * 0.39
       ret.steerRatio = 13.66 # 13.37 is spec
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 238], [0, 238]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.28], [0.07]]      
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.29], [0.07]]      
       ret.lateralTuning.pid.kf = 0.000025
       tire_stiffness_factor = 0.8467
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
